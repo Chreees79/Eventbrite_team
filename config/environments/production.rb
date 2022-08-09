a62ddbf0cb22aa3c93b55fa52e78be164ef78435
xko <germain.rgb@hotmail.fr>
2022-08-09 13:24:53 +0200

    test2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'eventbrite-team.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://eventbrite-team.herokuapp.com' }
 end
