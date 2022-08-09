4b6c3407e776c7fbaeabe532990e4ea8160cad64
xko <germain.rgb@hotmail.fr>
2022-08-09 13:16:50 +0200

    test2

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,13 +3,13 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    @url = 'http://monsite.fr/login'
+    @url = 'http://monsite.fr/login' #https://eventbrite-team.herokuapp.com/
     mail(to: @user.email, subject: 'Bienvenue chez nous !')
   end
 
   def attendances_email(admin)
     @user = admin
-    @url = 'http://eventbrite.fr/event'
+    @url = 'http://eventbrite.fr/event' #https://eventbrite-team.herokuapp.com/
     mail(to: @user.email, subject: 'Un nouveau participant à votre évènement de merde')
   end
 
