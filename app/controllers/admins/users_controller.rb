847156e00a27433ca199db12da30ea5e18b51f75
linahello <tropikalmoon@gmail.com>
2022-08-12 17:16:15 +0200

    friday youhou

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -20,6 +20,6 @@ class Admins::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :description, :email)
+    params.require(:user).permit(:first_name, :last_name, :description, :email, :app_admin)
   end
 end
--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,4 +1,5 @@
 class AdminsController < ApplicationController
+  before_action :authorize_admin, only: :index
   def index
     @users = User.all
     @events = Event.all
